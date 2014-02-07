437b8b90ebb6f5167019a13bb985a67eed74c99b
Subsentient <thinkingrodent@gmail.com>
2014-02-07 07:27:31 -0700

    Add game version to  command.

--- src/wz.c
+++ src/wz.c
@@ -177,10 +177,10 @@ Bool WZ_GetGamesList(const char *Server, unsigned short Port, const char *SendTo
 	/*Now send them to the user.*/
 	for (Inc = 0; Inc < GamesAvailable; ++Inc)
 	{
-		snprintf(OutBuf, sizeof OutBuf, "[Game %d] Name: %s | Map: %s %s|  Host: %s | Players: %d/%d %s| IP: %s",
+		snprintf(OutBuf, sizeof OutBuf, "[Game %d] Name: %s | Map: %s %s| Host: %s | Players: %d/%d %s| IP: %s | Version: %s",
 				Inc + 1, GamesList[Inc].GameName, GamesList[Inc].Map, GamesList[Inc].Mods ? "(mods required) " : "",
 				GamesList[Inc].HostNick, GamesList[Inc].NetSpecs.CurPlayers, GamesList[Inc].NetSpecs.MaxPlayers,
-				GamesList[Inc].PrivateGame ? "(private) " : "", GamesList[Inc].NetSpecs.HostIP);
+				GamesList[Inc].PrivateGame ? "(private) " : "", GamesList[Inc].NetSpecs.HostIP, GamesList[Inc].VersionString);
 		IRC_Message(SendTo, OutBuf);
 	}
 	
