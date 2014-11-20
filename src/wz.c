4d5ec26cce98ca194298eecbab25d3f149c85733
Subsentient <thinkingrodent@gmail.com>
2014-11-19 20:08:23 -0700

    Change game count format to avert confusion for 'wz'.

--- src/wz.c
+++ src/wz.c
@@ -267,7 +267,7 @@ Bool WZ_GetGamesList(const char *Server, unsigned short Port, const char *SendTo
 			snprintf(ModBuf, sizeof ModBuf, " \0034(mods: %s)\x3", GamesList[Inc].ModList);
 		}
 		
-		snprintf(OutBuf, sizeof OutBuf, "\2\3%s[%u/%u]\3\2 \02Name\02: %s | \02Map\02: %s | \02Host\02: %s | "
+		snprintf(OutBuf, sizeof OutBuf, "\2\3%s[%u of %u]\3\2 \02Name\02: %s | \02Map\02: %s | \02Host\02: %s | "
 				"\02Players\02: %d/%d %s| \02IP\02: %s | \02Version\02: %s%s",
 				ColTag, (unsigned)Inc + 1, (unsigned)GamesAvailable, GamesList[Inc].GameName, MapBuf,
 				GamesList[Inc].HostNick, GamesList[Inc].NetSpecs.CurPlayers, GamesList[Inc].NetSpecs.MaxPlayers,
