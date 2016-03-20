52dc355b866fb60337e789c21aa439bdde3ab9c7
Subsentient <thinkingrodent@gmail.com>
2016-03-20 13:45:27 -0700

    Remove unnecessary 'extern' keyword from function prototypes.

--- src/aqu4.h
+++ src/aqu4.h
@@ -116,91 +116,91 @@ struct _CmdList
 typedef enum { BLACK = 30, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, ENDCOLOR = 0 } ConsoleColor;
 
 /*main.c*/
-extern void Bot_SetTextColor(ConsoleColor Color);
-extern bool Main_SaveSocket(const char *OkMessageTarget);
+void Bot_SetTextColor(ConsoleColor Color);
+bool Main_SaveSocket(const char *OkMessageTarget);
 
 /*config.c*/
-extern bool Config_ReadConfig(void);
-extern bool Config_LoadBrain(void);
-extern bool Config_DumpBrain(void);
+bool Config_ReadConfig(void);
+bool Config_LoadBrain(void);
+bool Config_DumpBrain(void);
 
 /*netcore.c*/
-extern bool Net_Connect(const char *InHost, unsigned short PortNum, int *SocketDescriptor_);
-extern bool Net_Write(int SockDescriptor, const char *InMsg);
-extern bool Net_Read(int SockDescriptor, void *OutStream_, unsigned MaxLength, bool TextStream);
-extern bool Net_Disconnect(int SockDescriptor);
+bool Net_Connect(const char *InHost, unsigned short PortNum, int *SocketDescriptor_);
+bool Net_Write(int SockDescriptor, const char *InMsg);
+bool Net_Read(int SockDescriptor, void *OutStream_, unsigned MaxLength, bool TextStream);
+bool Net_Disconnect(int SockDescriptor);
 
 /*irc.c*/
-extern bool IRC_Connect(void);
-extern void IRC_Loop(void);
-extern bool IRC_Quit(const char *QuitMSG);
-extern bool IRC_JoinChannel(const char *Channel);
-extern bool IRC_LeaveChannel(const char *Channel);
-extern bool IRC_Message(const char *Target, const char *Message);
-extern bool IRC_Notice(const char *Target, const char *Notice);
-extern bool IRC_NickChange(const char *Nick);
-extern void IRC_Pong(const char *Param);
-extern struct ChannelTree *IRC_AddChannelToTree(const char *const Channel, const char *const Prefix);
-extern bool IRC_DelChannelFromTree(const char *Channel);
-extern void IRC_ShutdownChannelTree(void);
-extern MessageType IRC_GetMessageType(const char *InStream_);
-extern bool IRC_GetMessageData(const char *Message, char *OutData);
-extern bool IRC_BreakdownNick(const char *Message, char *NickOut, char *IdentOut, char *MaskOut);
-extern bool IRC_GetStatusCode(const char *Message, int *OutNumber);
-extern bool IRC_AddUserToChannel(const char *Channel, const char *Nick, const char *Ident, const char *Mask, bool FullUser);
-extern bool IRC_DelUserFromChannel(const char *Channel, const char *Nick);
-extern void IRC_CompleteChannelUser(const char *const Nick, const char *const Ident, const char *const Mask);
-extern bool IRC_DelUserFromChannelP(struct ChannelTree *const Channel, const char *const Nick);
-extern bool IRC_UserInChannel(const char *Channel, const char *Nick);
-extern bool IRC_UserInChannelP(const struct ChannelTree *Channel, const char *Nick);
-extern void IRC_ShutdownChannelUsers(struct ChannelTree *Channel);
-extern struct ChannelTree *IRC_GetChannelFromDB(const char *const Channel);
-extern bool IRC_StripControlCodes(char *Stream);
-extern struct _UserList *IRC_GetUserInChannel(const char *const ChannelName, const char *Nick_);
+bool IRC_Connect(void);
+void IRC_Loop(void);
+bool IRC_Quit(const char *QuitMSG);
+bool IRC_JoinChannel(const char *Channel);
+bool IRC_LeaveChannel(const char *Channel);
+bool IRC_Message(const char *Target, const char *Message);
+bool IRC_Notice(const char *Target, const char *Notice);
+bool IRC_NickChange(const char *Nick);
+void IRC_Pong(const char *Param);
+struct ChannelTree *IRC_AddChannelToTree(const char *const Channel, const char *const Prefix);
+bool IRC_DelChannelFromTree(const char *Channel);
+void IRC_ShutdownChannelTree(void);
+MessageType IRC_GetMessageType(const char *InStream_);
+bool IRC_GetMessageData(const char *Message, char *OutData);
+bool IRC_BreakdownNick(const char *Message, char *NickOut, char *IdentOut, char *MaskOut);
+bool IRC_GetStatusCode(const char *Message, int *OutNumber);
+bool IRC_AddUserToChannel(const char *Channel, const char *Nick, const char *Ident, const char *Mask, bool FullUser);
+bool IRC_DelUserFromChannel(const char *Channel, const char *Nick);
+void IRC_CompleteChannelUser(const char *const Nick, const char *const Ident, const char *const Mask);
+bool IRC_DelUserFromChannelP(struct ChannelTree *const Channel, const char *const Nick);
+bool IRC_UserInChannel(const char *Channel, const char *Nick);
+bool IRC_UserInChannelP(const struct ChannelTree *Channel, const char *Nick);
+void IRC_ShutdownChannelUsers(struct ChannelTree *Channel);
+struct ChannelTree *IRC_GetChannelFromDB(const char *const Channel);
+bool IRC_StripControlCodes(char *Stream);
+struct _UserList *IRC_GetUserInChannel(const char *const ChannelName, const char *Nick_);
 
 /*commands.c*/
-extern void CMD_ProcessCommand(const char *InStream);
-extern bool CMD_AddToTellDB(const char *Target, const char *Source, const char *Message);
-extern bool CMD_ReadTellDB(const char *Target);
-extern unsigned CMD_AddToStickyDB(const char *Owner, const char *Sticky, bool Private);
-extern void CMD_UpdateSeenDB(long Time, const char *Nick, const char *Channel, const char *LastMessage);
-extern bool CMD_SaveSeenDB(void);
-extern void CMD_LoadSeenDB(void);
-extern void CMD_AddUserMode(const char *Nick, const char *Ident, const char *Mask, const char *Mode,
+void CMD_ProcessCommand(const char *InStream);
+bool CMD_AddToTellDB(const char *Target, const char *Source, const char *Message);
+bool CMD_ReadTellDB(const char *Target);
+unsigned CMD_AddToStickyDB(const char *Owner, const char *Sticky, bool Private);
+void CMD_UpdateSeenDB(long Time, const char *Nick, const char *Channel, const char *LastMessage);
+bool CMD_SaveSeenDB(void);
+void CMD_LoadSeenDB(void);
+void CMD_AddUserMode(const char *Nick, const char *Ident, const char *Mask, const char *Mode,
 							const char *Target, const char *Channel);
-extern bool CMD_DelUserMode(const char *Nick, const char *Ident, const char *Mask, const char *Mode, const char *Target, const char *Channel);
-extern bool CMD_LoadUserModes(void);
-extern bool CMD_SaveUserModes(void);
-extern void CMD_ListUserModes(const char *SendTo);
-extern void CMD_ProcessUserModes(const char *Nick, const char *Ident, const char *Mask, const char *Channel);
+bool CMD_DelUserMode(const char *Nick, const char *Ident, const char *Mask, const char *Mode, const char *Target, const char *Channel);
+bool CMD_LoadUserModes(void);
+bool CMD_SaveUserModes(void);
+void CMD_ListUserModes(const char *SendTo);
+void CMD_ProcessUserModes(const char *Nick, const char *Ident, const char *Mask, const char *Channel);
 
 /*auth.c*/
-extern bool Auth_AddAdmin(const char *Nick, const char *Ident, const char *Mask, bool BotOwner);
-extern bool Auth_DelAdmin(const char *Nick, const char *Ident, const char *Mask, bool OwnersToo);
-extern bool Auth_IsAdmin(const char *Nick, const char *Ident, const char *Mask, bool *BotOwner);
-extern void Auth_ListAdmins(const char *SendTo);
-extern void Auth_ShutdownAdmin(void);
-extern bool Auth_IsBlacklisted(const char *Nick, const char *Ident, const char *Mask);
-extern bool Auth_BlacklistDel(const char *Nick, const char *Ident, const char *Mask);
-extern bool Auth_BlacklistAdd(const char *Nick, const char *Ident, const char *Mask);
-extern void Auth_ShutdownBlacklist(void);
-extern void Auth_BlacklistLoad(void);
-extern bool Auth_BlacklistSave(void);
-extern void Auth_BlacklistSendList(const char *SendTo);
+bool Auth_AddAdmin(const char *Nick, const char *Ident, const char *Mask, bool BotOwner);
+bool Auth_DelAdmin(const char *Nick, const char *Ident, const char *Mask, bool OwnersToo);
+bool Auth_IsAdmin(const char *Nick, const char *Ident, const char *Mask, bool *BotOwner);
+void Auth_ListAdmins(const char *SendTo);
+void Auth_ShutdownAdmin(void);
+bool Auth_IsBlacklisted(const char *Nick, const char *Ident, const char *Mask);
+bool Auth_BlacklistDel(const char *Nick, const char *Ident, const char *Mask);
+bool Auth_BlacklistAdd(const char *Nick, const char *Ident, const char *Mask);
+void Auth_ShutdownBlacklist(void);
+void Auth_BlacklistLoad(void);
+bool Auth_BlacklistSave(void);
+void Auth_BlacklistSendList(const char *SendTo);
 
 /*logging.c*/
-extern bool Log_WriteMsg(const char *InStream, MessageType MType);
-extern bool Log_CoreWrite(const char *InStream, const char *FileTitle);
-extern bool Log_TailLog(const char *const ChannelOrNick, const int NumLinesToOut, char *const OutStream, const int Capacity);
+bool Log_WriteMsg(const char *InStream, MessageType MType);
+bool Log_CoreWrite(const char *InStream, const char *FileTitle);
+bool Log_TailLog(const char *const ChannelOrNick, const int NumLinesToOut, char *const OutStream, const int Capacity);
 
 /*wz.c*/
-extern bool WZ_GetGamesList(const char *Server, unsigned short Port, const char *SendTo, bool WZLegacy);
+bool WZ_GetGamesList(const char *Server, unsigned short Port, const char *SendTo, bool WZLegacy);
 
 /*ddg.c*/
-extern bool DDG_Query(const char *Search, const char *SendTo);
+bool DDG_Query(const char *Search, const char *SendTo);
 
 /*curlcore.c*/
-extern bool CurlCore_GetHTTP(const char *const URL, void *const OutStream, const unsigned MaxOutBytes);
+bool CurlCore_GetHTTP(const char *const URL, void *const OutStream, const unsigned MaxOutBytes);
 
 /*Various globals.*/
 extern int SocketDescriptor;
